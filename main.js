c98d5b057a6c282a968b4b6f0bed45a7db5c595b
simerdeep92 <sjolly@ncsu.edu>
2016-11-21 18:56:52 -0500

    test cases

--- main.js
+++ main.js
@@ -1,35 +1,11 @@
-var express = require('express')
-var fs      = require('fs')
-var app = express()
-var sys = require('sys')
-var exec = require('child_process').exec;
-var redis = require('redis');
+var express = require('express');
+var app = express();
+var exports = module.exports = {};
 
-app.get('/', function(req, res) 
-{
-	res.send("Hello!!This is green Build for devops-HW4!");
+app.get('/', function(req, res){
+  res.send('Hello World');
 });
 
-var server = app.listen(3000, function () {
-
-	var host = server.address().address
-	var port = server.address().port
-
-	console.log('Example app listening at http://%s:%s', host, port)
+var server = app.listen(3000, function(){
+  console.log('Magic is happening on port 3000');
 });
-
-var portrange = 6500
-var containers = {}
-var http = require('http')
-var proxy = require('http-proxy')
-
-app.get('/spawn', function(req, res) { 
-		res.send("Container is spawned ");
-        var proxyServer = proxy.createProxyServer({})
-        proxyServer.web(req, res, {
-		    target: "http://172.17.0.1:3002/"
-		    });
-    })
-
-
-
