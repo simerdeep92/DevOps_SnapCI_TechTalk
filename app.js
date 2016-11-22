324683f867ff361c51bd7f360f91fd23a90e8876
simerdeep92 <sjolly@ncsu.edu>
2016-11-21 22:30:11 -0500

    changes

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', function(req, res){
     res.send('Hello World');
 });
 
-var server = app.listen(80, function(){
-    console.log('Magic is happening on port 80');
+var server = app.listen(8080, function(){
+    console.log('Magic is happening on port 8080');
 });
